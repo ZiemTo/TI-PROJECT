2832e6b85a1a765b8a298545ceefc9403c76b3f4
ZiemTo <151959047+ZiemTo@users.noreply.github.com>
2024-01-24 01:46:25 +0100

    Local Storage
    Fixed list, added saving to local storage, added slider with backgrounds, added cookies with default timers

--- script.js
+++ script.js
@@ -1,5 +1,83 @@
 
+var background;
+
+
 window.addEventListener("resize",ResizeTextareawindow);
+function handleMutation(mutationsList, observer) {
+    mutationsList.forEach((mutation) => {
+        let savetasks=document.querySelectorAll(".visible-span>label>textarea")
+        var values=[];
+        i=0;
+        savetasks.forEach((savetask)=>
+        {
+            if(!savetask.value.length<1 &&!savetask.parentElement.previousSibling.previousSibling.checked){
+
+        
+            values[i]=savetask.value;
+            i++;
+        }
+        
+   })
+   values=JSON.stringify(values);
+   localStorage.setItem("savedtasks",values)
+    });
+}
+const observer = new MutationObserver(handleMutation);
+
+const targetElement = document.querySelector(".list");
+
+const config = {  characterData: true, attributes: true, subtree: true  };
+const targetTextarea = document.querySelectorAll("textarea");
+const targetCheckbox = document.querySelectorAll("input[type=\"checkbox\"]");
+
+
+observer.observe(targetElement, config);
+function SetPrevious()
+{
+    if(localStorage.getItem("background")!=null)
+    {
+        document.querySelector("#background").setAttribute("src",localStorage.getItem("background"));
+    }
+    if(localStorage.getItem("savedtasks")!=null)
+    {
+        let savedtasks = JSON.parse(localStorage.getItem("savedtasks"));
+        savedtasks.forEach((task)=>
+        {
+            const newspan = document.createElement("span");
+        newspan.setAttribute("class","spantask visible-span")
+        newspan.innerHTML = "<input type=\"checkbox\" name=\"task\" onclick=\"Cross(this)\" class=\"taskcheck \"> <label for=\"task\"><textarea class=\"task\"  maxlength=\"80\" oninput=\"ResizeTextarea(this)\" placeholder=\"Write your task here\" ></textarea></label>";
+        document.querySelector("#tasks").appendChild(newspan);
+        newspan.firstChild.nextSibling.nextSibling.firstChild.value=task;
+        })
+        
+    }
+    if(getCookie("minutes").length>0)
+    {
+        
+        document.querySelector("#time").firstChild.nextSibling.setAttribute("value",getCookie("minutes"));
+    }
+    if(getCookie("seconds").length>0)
+    {
+        
+        document.querySelector("#time").lastChild.previousSibling.setAttribute("value",getCookie("seconds"));
+    }
+    
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 function ResizeTextareawindow()
 {
     const tasks = document.querySelectorAll("textarea");
@@ -7,6 +85,7 @@ function ResizeTextareawindow()
     {
         ResizeTextarea(task)
     }) 
+    
    
 }
 
@@ -21,6 +100,8 @@ function ResizeTextarea(task)
     }
     task.setAttribute("style", "height:" + (task.scrollHeight) + "px;");
     task.addEventListener("input", OnInput, false);
+   
+   
 }
 function OnInput() {
     this.style.height = "auto";
@@ -39,6 +120,7 @@ function AddTask()
         if(document.querySelector("#btn1").getAttribute("class")=="hidden-span"){
         checkbox.addEventListener('change',HideTasks)}
         })
+        
         
 }
 
@@ -77,17 +159,18 @@ function ShowTasks()
 }
 function Cross(checkbox)
 {
-  console.log
     if(checkbox.checked)
     {
         
         checkbox.nextSibling.nextSibling.firstChild.classList.add("crossed")
         ResizeTextarea(checkbox.nextSibling.nextSibling.firstChild);
+        
     }
     else{
     
         checkbox.nextSibling.nextSibling.firstChild.classList.remove("crossed")
     }
+    
 }
 function Close()
 {
@@ -103,17 +186,19 @@ function openNav()
 }
 function ShowContent(li)
 {
-    console.log(li.firstChild.innerHTML)
+    
     if(li.firstChild.innerHTML=="&gt;")
     { 
         li.firstChild.innerHTML="&or;"
         li.nextSibling.nextSibling.classList.add("visible-span");
+        
         li.nextSibling.nextSibling.classList.remove("hidden-span");
     }
 
     else {
         li.firstChild.innerHTML="&gt;"
         li.nextSibling.nextSibling.classList.add("hidden-span");
+        
         li.nextSibling.nextSibling.classList.remove("visible-span");
     }
     
@@ -129,4 +214,24 @@ function Pause()
     document.querySelector(".time:first-child").removeAttribute("style")
     document.querySelector(".time:last-child").removeAttribute("style")
 }
+function SetBackground(a)
+{
+    div = document.querySelector(a.getAttribute("href"))
+    src = div.firstChild.nextSibling.getAttribute("src")
+    document.querySelector("#background").setAttribute("src",src);
+    localStorage.setItem("background",src);
+}
+function SetDefaultValues()
+{
+    const cookieMinutes = "minutes";
+    const cookieSeconds = "seconds";
+ 
+    const cookieMinutesValue = document.querySelector("#defaulttimer").firstChild.nextSibling.value;
+    const cookieSecondsValue = document.querySelector("#defaulttimer").lastChild.previousSibling.value;
+    
+    const exp = new Date(2147483647 * 1000).toUTCString();
+    document.cookie = cookieMinutes + '=' + cookieMinutesValue + ';expires=' + exp;
+    document.cookie = cookieSeconds + '=' + cookieSecondsValue + ';expires=' + exp;
+}
+
 
